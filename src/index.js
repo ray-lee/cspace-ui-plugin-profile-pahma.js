7984449b436670306c7e3ff4f83d6d31851c50d0
Ray Lee <rhlee@berkeley.edu>
2018-06-12 23:15:34 -0700

    PAHMA-1658: Create skeletal cspace-ui plugin.

---
+++ src/index.js
@@ -0,0 +1,29 @@
+/* global cspaceUIPluginProfilePAHMA */
+/* The cspaceUIPluginProfilePAHMA global variable is set by webpack (in non-test builds). See
+ * webpack.config.js. */
+
+import { defineMessages } from 'react-intl';
+import logo from '../images/logo.svg';
+import messages from './messages';
+import plugins from './plugins';
+import styles from '../styles/cspace-ui-plugin-profile/pahma.css';
+
+module.exports = () => ({
+  logo,
+  messages,
+  className: styles.common,
+  prettyUrls: true,
+  tenantId: '15',
+  pluginInfo: {
+    cspaceUIPluginProfilePAHMA: {
+      messages: defineMessages({
+        name: {
+          id: 'cspaceUIPluginProfilePAHMA.name',
+          defaultMessage: 'PAHMA profile',
+        },
+      }),
+      version: cspaceUIPluginProfilePAHMA.packageVersion,
+    },
+  },
+  plugins: plugins.map(plugin => plugin()),
+});
---
+++ src/messages.js
@@ -0,0 +1,15 @@
+export default {
+  'about.title': 'PAHMA CollectionSpace',
+  'about.contentHTML': `
+    <p>
+      Collections management system of the Phoebe A. Hearst Museum of Anthropology.
+    </p>
+    <p>
+      This service is supported by UC Berkeley <a href="http://research-it.berkeley.edu/">Research IT</a>.
+      To send feedback, report bugs, or request support, email
+      <a href="mailto:cspace-support@lists.berkeley.edu">cspace-support@lists.berkeley.edu</a>.
+    </p>`,
+
+  'footer.feedback': 'Contact UC Berkeley CollectionSpace support',
+  'footer.feedbackUrl': 'mailto:cspace-support@lists.berkeley.edu',
+};
