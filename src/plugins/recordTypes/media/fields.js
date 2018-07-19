e248168f7d7b9e18453debde394e7d3ad752edaf
Ray Lee <rhlee@berkeley.edu>
2018-07-18 23:07:13 -0700

    PAHMA-1676: Remove source prop from identificationNumber.

--- src/plugins/recordTypes/media/fields.js
+++ src/plugins/recordTypes/media/fields.js
@@ -23,6 +23,7 @@ export default (configContext) => {
             required: false,
             view: {
               type: TextInput,
+              props: null,
             },
           },
         },
