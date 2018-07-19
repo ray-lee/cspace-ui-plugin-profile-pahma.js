9eb5f5f438b1ef877a4dc22f9a765313db60c794
Cesar Villalobos <cesarv.h@berkeley.edu>
2018-07-18 18:31:41 -0700

    PAHMA-1687: Added title

---
+++ src/plugins/recordTypes/claim/title.js
@@ -0,0 +1,17 @@
+export default configContext => (data) => {
+  const {
+    getPart,
+  } = configContext.recordDataHelpers;
+
+  if (!data) {
+    return '';
+  }
+
+  const common = getPart(data, 'claims_common');
+  const anthro = getPart(data, 'claims_anthropology');
+
+  const claimNumber = common ? common.get('claimNumber') : '';
+  const claimName = anthro ? anthro.get('claimName') : '';
+
+  return [claimNumber, claimName].filter(part => !!part).join(' – ');
+};
