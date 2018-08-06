0b71cabeb3969cb7e6d610bbcf885e057af688a8
Cesar Villalobos <cesarv.h@berkeley.edu>
2018-08-06 12:48:02 -0700

    PAHMA-1679: Fixed pahmaIntake generator message and csid

--- src/plugins/recordTypes/intake/idGenerators.js
+++ src/plugins/recordTypes/intake/idGenerators.js
@@ -2,11 +2,11 @@ import { defineMessages } from 'react-intl';
 
 export default {
   pahmaIntake: {
-    csid: '8088cfa5-c743-4824-bb4d-fb11b12847f7',
+    csid: '83eda2f2-5f29-4d4a-8217-dc1d9f5ce0eb',
     messages: defineMessages({
       type: {
         id: 'idGenerator.pahmaIntake.type',
-        defaultMessage: 'Intake',
+        defaultMessage: 'Entry',
       },
     }),
   },
