25d31df046b218b0104effba601d40b1d756e03b
cesarvh <cesarv.h@berkeley.edu>
2019-08-07 14:45:32 -0700

    CC-489: Added MergeAuthorityItems sources

---
+++ src/plugins/invocables/batch/org.collectionspace.services.batch.nuxeo.MergeAuthorityItemsBatchJob/index.js
@@ -0,0 +1,11 @@
+import fields from './fields';
+
+export default () => configContext => ({
+  invocables: {
+    batch: {
+      'org.collectionspace.services.batch.nuxeo.MergeAuthorityItemsBatchJob': {
+        fields: fields(configContext),
+      },
+    },
+  },
+});
