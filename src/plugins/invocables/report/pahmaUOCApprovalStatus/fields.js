1a3ee0e69d404d9574340ffdb0a38748d4a359ea
cesarvh <cesarv.h@berkeley.edu>
2019-08-14 14:01:09 -0700

    CC-489: Removed 'Requestor' as a required field

--- src/plugins/invocables/report/pahmaUOCApprovalStatus/fields.js
+++ src/plugins/invocables/report/pahmaUOCApprovalStatus/fields.js
@@ -25,7 +25,6 @@ export default (configContext) => {
               defaultMessage: 'Requester',
             },
           }),
-          required: true,
           view: {
             type: TextInput,
           },
