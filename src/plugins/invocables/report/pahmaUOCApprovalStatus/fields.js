8b3faa563759c483445fb4a13e453e8594517832
cesarvh <cesarv.h@berkeley.edu>
2019-08-16 14:39:01 -0400

    CC-728: Added various parameters to UOCApprovalStatus report

--- src/plugins/invocables/report/pahmaUOCApprovalStatus/fields.js
+++ src/plugins/invocables/report/pahmaUOCApprovalStatus/fields.js
@@ -4,6 +4,10 @@ export default (configContext) => {
   const {
     CompoundInput,
     OptionPickerInput,
+    TermPickerInput,
+    DateInput,
+    TextInput,
+    AutocompleteInput,
   } = configContext.inputComponents;
 
   const {
@@ -23,14 +27,86 @@ export default (configContext) => {
           messages: defineMessages({
             name: {
               id: 'field.report.pahmaUOCApprovalStatus.OutputMIME.name',
-              defaultMessage: 'Format as',
+              defaultMessage: 'Output format',
             },
           }),
+          required: true,
           view: {
             type: OptionPickerInput,
             props: {
               source: 'uocApprovalStatusMIMETypes',
             },
+          },
+        },
+      },
+      Requester: {
+        [config]: {
+          messages: defineMessages({
+            name: {
+              id: 'field.report.pahmaUOCApprovalStatus.Requester.name',
+              defaultMessage: 'Requester',
+            },
+          }),
+          view: {
+            type: TextInput,
+          },
+        },
+      },
+      AuthBy: {
+        [config]: {
+          messages: defineMessages({
+            name: {
+              id: 'field.report.pahmaUOCApprovalStatus.AuthBy.name',
+              defaultMessage: 'Authorized by',
+            },
+          }),
+          view: {
+            type: AutocompleteInput,
+            props: {
+              source: 'person/local,person/shared,organization/local,organization/shared',
+            },
+          },
+        },
+      },
+      AuthStatus: {
+        [config]: {
+          messages: defineMessages({
+            name: {
+              id: 'field.report.pahmaUOCApprovalStatus.AuthStatus.name',
+              defaultMessage: 'Authorization status',
+            },
+          }),
+          view: {
+            type: TermPickerInput,
+            props: {
+              source: 'uocauthorizationstatuses',
+            },
+          },
+        },
+      },
+      StartDate: {
+        [config]: {
+          messages: defineMessages({
+            name: {
+              id: 'field.report.pahmaUOCApprovalStatus.StartDate.name',
+              defaultMessage: 'Earliest requested date',
+            },
+          }),
+          view: {
+            type: DateInput,
+          },
+        },
+      },
+      EndDate: {
+        [config]: {
+          messages: defineMessages({
+            name: {
+              id: 'field.report.pahmaUOCApprovalStatus.EndDate.name',
+              defaultMessage: 'Latest requested date',
+            },
+          }),
+          view: {
+            type: DateInput,
           },
         },
       },
--- src/plugins/invocables/report/pahmaUOCApprovalStatus/forms/default.jsx
+++ src/plugins/invocables/report/pahmaUOCApprovalStatus/forms/default.jsx
@@ -9,8 +9,21 @@ const template = (configContext) => {
     Field,
   } = configContext.recordComponents;
 
+  const {
+    Row,
+  } = configContext.layoutComponents;
+
   return (
     <Field name="document">
+      <Field name="Requester" />
+      <Row>
+        <Field name="AuthStatus" />
+        <Field name="AuthBy" />
+      </Row>
+      <Row>
+        <Field name="StartDate" />
+        <Field name="EndDate" />
+      </Row>
       <Field name="OutputMIME" />
     </Field>
   );
