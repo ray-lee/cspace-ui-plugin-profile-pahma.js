da6a281c062f38f5035704299e6bda78b2af7a63
cesarvh <cesarv.h@berkeley.edu>
2020-02-06 11:48:29 -0800

    CC-1000: Moved collectionRegion to Core

--- src/plugins/recordTypes/uoc/fields.js
+++ src/plugins/recordTypes/uoc/fields.js
@@ -1,9 +1,6 @@
-import { defineMessages } from 'react-intl';
-
 export default (configContext) => {
   const {
     TextInput,
-    TermPickerInput,
   } = configContext.inputComponents;
 
   const {
@@ -17,29 +14,6 @@ export default (configContext) => {
           [config]: {
             view: {
               type: TextInput,
-            },
-          },
-        },
-      },
-      'ns2:uoc_pahma': {
-        [config]: {
-          service: {
-            ns: 'http://collectionspace.org/services/uoc/local/pahma',
-          },
-        },
-        collectionRegion: {
-          [config]: {
-            messages: defineMessages({
-              name: {
-                id: 'field.uoc_pahma.collectionRegion.name',
-                defaultMessage: 'Region or collection',
-              },
-            }),
-            view: {
-              type: TermPickerInput,
-              props: {
-                source: 'collectionRegions',
-              },
             },
           },
         },
--- src/plugins/recordTypes/uoc/index.js
+++ src/plugins/recordTypes/uoc/index.js
@@ -1,11 +1,9 @@
 import fields from './fields';
-import forms from './forms';
 
 export default () => configContext => ({
   recordTypes: {
     uoc: {
       fields: fields(configContext),
-      forms: forms(configContext),
     },
   },
 });
