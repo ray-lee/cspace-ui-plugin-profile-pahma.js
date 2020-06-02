3adf0522d2cada2e4d358200e6737f83e3c005fe
Lam Voong <lkv@berkeley.edu>
2020-06-02 09:49:38 -0700

    CC-1181: Add new parameters for pahmaUoCbyStudentVisits

---
+++ src/plugins/invocables/report/pahmaUoCbyStudentVisits/fields.js
@@ -0,0 +1,66 @@
+import { defineMessages } from 'react-intl';
+
+export default (configContext) => {
+  const {
+    CompoundInput,
+    AutocompleteInput,
+    DateInput,
+  } = configContext.inputComponents;
+
+  const {
+    configKey: config,
+  } = configContext.configHelpers;
+
+  return {
+    params: {
+      [config]: {
+        view: {
+          type: CompoundInput,
+        },
+      },
+      StaffName: {
+        [config]: {
+          messages: defineMessages({
+            name: {
+              id: 'field.pahmaUoCbyStudentVisits.Requester.name',
+              defaultMessage: 'Staff Name',
+            },
+          }),
+          view: {
+            type: AutocompleteInput,
+            props: {
+              source: 'person/local',
+              showQuickAdd: false,
+            },
+          },
+        },
+      },
+      VisitStartDate: {
+        [config]: {
+          messages: defineMessages({
+            name: {
+              id: 'field.pahmaUoCbyStudentVisits.VisitStartDate.name',
+              defaultMessage: 'Visit Start Date',
+            },
+          }),
+          view: {
+            type: TextInput,
+          },
+        },
+      },
+      VisitEndDate: {
+        [config]: {
+          messages: defineMessages({
+            name: {
+              id: 'field.pahmaUoCbyStudentVisits.VisitEndDate.name',
+              defaultMessage: 'Visit End Date',
+            },
+          }),
+          view: {
+            type: TextInput,
+          },
+        },
+      },
+    },
+  };
+};
