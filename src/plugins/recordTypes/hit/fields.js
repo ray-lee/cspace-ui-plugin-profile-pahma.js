a7ff56f42a6155500875b3c123d65184c28b4330
cesarvh <cesarv.h@berkeley.edu>
2021-05-11 13:52:18 -0700

    CC-1660: Added pahma HIT customizations

--- src/plugins/recordTypes/hit/fields.js
+++ src/plugins/recordTypes/hit/fields.js
@@ -1,11 +1,26 @@
+import { defineMessages } from 'react-intl';
+
 export default (configContext) => {
   const {
+    DateInput,
     TextInput,
+    CompoundInput,
+    TermPickerInput,
+    StructuredDateInput
   } = configContext.inputComponents;
+
+  const {
+    DATA_TYPE_DATE,
+    DATA_TYPE_STRUCTURED_DATE
+  } = configContext.dataTypes;
 
   const {
     configKey: config,
   } = configContext.configHelpers;
+
+  const {
+    extensions,
+  } = configContext.config;
 
   return {
     document: {
@@ -14,6 +29,7 @@ export default (configContext) => {
           [config]: {
             view: {
               type: TextInput,
+              props: null,
             },
           },
         },
@@ -38,6 +54,113 @@ export default (configContext) => {
                   props: {
                     source: 'pahmahitextapprovaltypes',
                   },
+                },
+              },
+            },
+          },
+        },
+      },
+      'ns2:hits_pahma': {
+        [config]: {
+          service: {
+            ns: 'http://collectionspace.org/services/hit/local/pahma',
+          },
+        },
+        pahmaAgreementRenewalDates: {
+          [config]: {
+            view: {
+              type: CompoundInput,
+            },
+          },
+          pahmaAgreementRenewalDate: {
+            [config]: {
+              messages: defineMessages({
+                name: {
+                  id: 'field.hits_pahma.pahmaAgreementRenewalDate.name',
+                  defaultMessage: 'Agreement renewal date',
+                },
+              }),
+              repeating: true,
+              view: {
+                type: TextInput,
+              },
+            },
+          },
+        },
+        pahmaHitAgreementGroupList: {
+          [config]: {
+            view: {
+              type: CompoundInput,
+            },
+          },
+          pahmaHitAgreementGroup: {
+            [config]: {
+              messages: defineMessages({
+                name: {
+                  id: 'field.hits_pahma.pahmaHitAgreementGroup.name',
+                  defaultMessage: 'Agreement status',
+                },
+              }),
+              repeating: true,
+              view: {
+                type: CompoundInput,
+                props: {
+                  tabular: true,
+                },
+              },
+            },
+            pahmaAgreementStatus: {
+              [config]: {
+                messages: defineMessages({
+                  fullName: {
+                    id: 'field.hits_pahma.pahmaAgreementStatus.fullName',
+                    defaultMessage: 'Agreement status',
+                  },
+                  name: {
+                    id: 'field.hits_pahma.pahmaAgreementStatus.name',
+                    defaultMessage: 'Status',
+                  },
+                }),
+                view: {
+                  type: TermPickerInput,
+                  props: {
+                    source: 'pahmaagreementstatuses',
+                  },
+                },
+              },
+            },
+            pahmaAgreementStatusDate: {
+              [config]: {
+                dataType: DATA_TYPE_DATE,
+                messages: defineMessages({
+                  fullName: {
+                    id: 'field.hits_pahma.pahmaAgreementStatusDate.fullName',
+                    defaultMessage: 'Agreement status date',
+                  },
+                  name: {
+                    id: 'field.hits_pahma.pahmaAgreementStatusDate.name',
+                    defaultMessage: 'Date',
+                  },
+                }),
+                view: {
+                  type: DateInput,
+                },
+              },
+            },
+            pahmaAgreementStatusNote: {
+              [config]: {
+                messages: defineMessages({
+                  fullName: {
+                    id: 'field.hits_pahma.pahmaAgreementStatusNote.fullName',
+                    defaultMessage: 'Agreement status note',
+                  },
+                  name: {
+                    id: 'field.hits_pahma.pahmaAgreementStatusNote.name',
+                    defaultMessage: 'Note',
+                  },
+                }),
+                view: {
+                  type: TextInput,
                 },
               },
             },
